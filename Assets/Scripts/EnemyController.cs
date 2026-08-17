2864efdc1921c412f4e675eb6acaf94404c7e9ea
A_Zael <kunwong2547@gmail.com>
2026-08-17 16:40:49 +0700

    Make fist Animation ezez

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    float speed = 1f;
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed;
+        rb.linearVelocity = new Vector3(forwardMove.x, rb.linearVelocity.y, forwardMove.z);
+    }
+}
