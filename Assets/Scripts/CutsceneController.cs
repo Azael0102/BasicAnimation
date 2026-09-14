06754fc415f826d44467d26f2b30f7eefd6bdc2f
A_Zael <kunwong2547@gmail.com>
2026-09-14 16:59:20 +0700

    Update
    Add Waypoint
    Add Cutscene
    Add NPC animation

---
+++ Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneController : MonoBehaviour
+{
+    [SerializeField] PlayableDirector director;
+    [SerializeField] GameObject canvas;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        if (canvas != null) canvas.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if (director != null)
+        {
+            director.stopped += OnCutsceneFinished;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (director != null)
+        {
+            director.stopped -= OnCutsceneFinished;
+        }
+    }
+
+    private void OnCutsceneFinished(PlayableDirector pd)
+    {
+        if (canvas != null)
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,20 +1,76 @@
 using System.Collections.Specialized;
 using System.Security.Cryptography;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
-    float speed = 1f;
-    Rigidbody rb;
+    float speed = 10f;
+    //Rigidbody rb;
+    NavMeshAgent agent;
+    Animator anim;
+
+    [SerializeField]
+    List<Transform> waypoint = new List<Transform>();
+    [SerializeField]
+    float waitTimeAtpoint = 2f;
+    [SerializeField]
+    bool patrolInLoop = true;
+
+    int currentWaypointIndex = 0;
+    bool isWaiting = false;
+    bool movingForward = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        if (waypoint == null || waypoint.Count == 0) return;
+
+        GoToCurrentWaypoint();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        Vector3 forwardMove = transform.forward * speed;
-        rb.linearVelocity = new Vector3(forwardMove.x, rb.linearVelocity.y, forwardMove.z);
+        if (waypoint.Count == 0 || isWaiting) return;
+
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            anim.SetTrigger("Stop");
+            agent.speed = 0;
+            StartCoroutine(WaitAtWaypoint());
+        }
+    }
+
+    void GoToCurrentWaypoint()
+    {
+        if (waypoint.Count == 0) return;
+        agent.SetDestination(waypoint[currentWaypointIndex].position);
+    }
+
+    void SelectNextWaypoint()
+    {
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoint.Count;
+    }
+
+    IEnumerator WaitAtWaypoint()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTimeAtpoint);
+        SelectNextWaypoint();
+        anim.SetTrigger("Walk");
+        agent.speed = speed;
+        GoToCurrentWaypoint();
+        isWaiting = false;
+    }
+
+        void FixedUpdate()
+    {
+        //Vector3 forwardMove = transform.forward * speed;
+        //rb.linearVelocity = new Vector3(forwardMove.x, rb.linearVelocity.y, forwardMove.z);
     }
 }
